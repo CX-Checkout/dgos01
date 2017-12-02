09e956fe89fb4a84c66e878b004164df7bf2b2f5
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-02 18:52:09 +0000

    Sat Dec 02 18:52:09 GMT 2017

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -4,18 +4,29 @@ import java.util.HashMap;
 
 public class Checkout {
     public static Integer checkout(String skus) {
-        HashMap<Character, Integer> prices = new HashMap<Character, Integer>();
-        prices.put('A', 50);
-        prices.put('B', 30);
-        prices.put('C', 20);
+        HashMap<Character, Integer> pricePerProduct = new HashMap<Character, Integer>();
+        pricePerProduct.put('A', 50);
+        pricePerProduct.put('B', 30);
+        pricePerProduct.put('C', 20);
 
-        HashMap<Character, Offer> offersByProduct = new HashMap<Character, Offer>();
-        offersByProduct.put('A', new Offer(3, ));
+        HashMap<Character, Offer> offerPerProduct = new HashMap<Character, Offer>();
+        offerPerProduct.put('A', new Offer(3, 130));
+
+        HashMap<Character, Integer> quantityPerProduct = new HashMap<>();
 
         if (skus.length() == 0) return 0;
-        int total = 0;
         for (char item: skus.toCharArray()) {
-            total += prices.get(item);
+            if (!quantityPerProduct.containsKey(item)) {
+                quantityPerProduct.put(item, 1);
+                continue;
+            }
+            int quantity = quantityPerProduct.get(item);
+            quantityPerProduct.replace(item, quantity + 1);
+        }
+
+        int total = 0;
+        for (char item: quantityPerProduct.keySet()) {
+            total += quantityPerProduct.get(item) * pricePerProduct.get(item);
         }
         return total;
     }
--- src/main/java/befaster/solutions/Offer.java
+++ src/main/java/befaster/solutions/Offer.java
@@ -1,4 +1,11 @@
 package befaster.solutions;
 
 public class Offer {
+    private final int quantity;
+    private final int price;
+
+    public Offer(int quantity, int price) {
+        this.quantity = quantity;
+        this.price = price;
+    }
 }

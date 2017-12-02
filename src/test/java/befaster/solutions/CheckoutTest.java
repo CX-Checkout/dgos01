aa07d1ad37bfb7f5b6a10f846be58b2bdd369974
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-02 18:11:41 +0000

    Sat Dec 02 18:11:41 GMT 2017

--- src/test/java/befaster/solutions/CheckoutTest.java
+++ src/test/java/befaster/solutions/CheckoutTest.java
@@ -2,8 +2,12 @@ package befaster.solutions;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class CheckoutTest {
     @Test
-    public void name() {
+    public void calculate_a_price_of_0_when_no_products_in_basket() {
+        assertThat(Checkout.checkout(""), is(0));
     }
 }

7e77e3c37cb628833800126ca8dfd9a8934b4ae8
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-02 17:41:52 +0000

    Sat Dec 02 17:41:52 GMT 2017

--- src/test/java/befaster/solutions/HelloTest.java
+++ src/test/java/befaster/solutions/HelloTest.java
@@ -2,9 +2,12 @@ package befaster.solutions;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class HelloTest {
     @Test
     public void should_return_a_message() {
-        assertThat()
+        assertThat(Hello.hello("NO_MATTERS"), is("A MESSAGE"));
     }
 }

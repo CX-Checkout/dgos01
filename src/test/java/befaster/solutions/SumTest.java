871ebcd787eab3d42c0637a41fb7ae2e69a55e6c
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-02 17:32:24 +0000

    Sat Dec 02 17:32:24 GMT 2017

--- src/test/java/befaster/solutions/SumTest.java
+++ src/test/java/befaster/solutions/SumTest.java
@@ -11,4 +11,14 @@ public class SumTest {
     public void compute_sum() {
         assertThat(Sum.sum(1, 1), equalTo(2));
     }
+
+    @Test
+    public void compute_sum_with_0_to_100_numbers() {
+        assertThat(Sum.sum(1, 10), equalTo(11));
+    }
+
+    @Test
+    public void throw_error_when_numbers_not_between_1_to_100() {
+        assertThat(Sum.sum(1, 10), equalTo(11));
+    }
 }

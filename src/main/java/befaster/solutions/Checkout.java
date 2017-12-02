5d0058516252c9dc2fb0366990de37d06cd568be
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-02 18:37:09 +0000

    Sat Dec 02 18:37:09 GMT 2017

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -1,12 +1,18 @@
 package befaster.solutions;
 
+import java.util.HashMap;
+
 public class Checkout {
     public static Integer checkout(String skus) {
-        if (skus.length() == 0) return 0;
-        for (:
-        skus.toCharArray()) {
+        HashMap<Character, Integer> prices = new HashMap<Character, Integer>();
+        prices.put('A', 50);
+        prices.put('B', 30);
 
+        if (skus.length() == 0) return 0;
+        int total = 0;
+        for (char item: skus.toCharArray()) {
+            total += prices.get(item);
         }
-        return 50;
+        return total;
     }
 }

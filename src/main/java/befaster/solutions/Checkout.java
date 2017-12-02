1742bb49b969376b01249c571d3ab64e26d36ce4
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-02 18:16:36 +0000

    Sat Dec 02 18:16:36 GMT 2017

--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -3,6 +3,10 @@ package befaster.solutions;
 public class Checkout {
     public static Integer checkout(String skus) {
         if (skus.length() == 0) return 0;
+        for (:
+        skus.toCharArray()) {
+
+        }
         return 50;
     }
 }

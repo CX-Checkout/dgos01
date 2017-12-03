06d1cd1adfb3afefedb2b39d5c8f74682adce1d1
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 16:13:13 +0000

    Sun Dec 03 16:13:13 GMT 2017

--- src/main/java/befaster/solutions/ProductDiscount.java
+++ src/main/java/befaster/solutions/ProductDiscount.java
@@ -1,7 +1,26 @@
 package befaster.solutions;
 
-public class ProductDiscount {
+public class ProductDiscount extends Discount {
+    private final int numberOfItems;
+    private final char productToDiscount;
+
     public ProductDiscount(int numberOfItems, char productToDiscount) {
-        super();
+        this.numberOfItems = numberOfItems;
+        this.productToDiscount = productToDiscount;
+    }
+
+    @Override
+    public boolean apply(int numberOfItems) {
+        return false;
+    }
+
+    @Override
+    public int getAmountToDiscount(int numberOfItemsConsidered) {
+        return 0;
+    }
+
+    @Override
+    public int getNumberOfAffectedItems(int numberOfItemsConsidered) {
+        return 0;
     }
 }

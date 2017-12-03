491494b8c106914f25c237596d1d597bcce11d6b
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 09:31:39 +0000

    Sun Dec 03 09:31:39 GMT 2017

--- src/main/java/befaster/solutions/Basket.java
+++ src/main/java/befaster/solutions/Basket.java
@@ -1,7 +1,29 @@
 package befaster.solutions;
 
-public class Basket {
-    public Basket(String skus) {
+import java.util.HashMap;
+import java.util.Set;
 
+public class Basket {
+
+    HashMap<Character, Integer> quantityPerProduct;
+
+    public Basket(String skus) {
+        quantityPerProduct = new HashMap<>();
+        for (char item: skus.toCharArray()) {
+            if (!quantityPerProduct.containsKey(item)) {
+                quantityPerProduct.put(item, 1);
+                continue;
+            }
+            int quantity = quantityPerProduct.get(item);
+            quantityPerProduct.replace(item, quantity + 1);
+        }
+    }
+
+    public Set<Character> getItems() {
+        return quantityPerProduct.keySet();
+    }
+
+    public int getQuantityFor(char item) {
+        return quantityPerProduct.get(item);
     }
 }

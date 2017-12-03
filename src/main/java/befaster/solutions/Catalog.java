540fdae9b2a7a37cfe5a34291b8cc77a62d33e66
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 21:37:01 +0000

    Sun Dec 03 21:37:01 GMT 2017

--- src/main/java/befaster/solutions/Catalog.java
+++ src/main/java/befaster/solutions/Catalog.java
@@ -68,7 +68,7 @@ public class Catalog {
         PackDiscount packDiscount = new PackDiscount(3, 45, 'Z', 'S', 'T', 'Y', 'X');
 
         productInfo = new ProductInfo(20);
-        productInfoº.addPackDiscount(packDiscount);
+        productInfo.addPackDiscount(packDiscount);
         values.put('S', productInfo);
 
         productInfo = new ProductInfo(20);
@@ -85,9 +85,18 @@ public class Catalog {
         values.put('V', productInfo);
 
         values.put('W', new ProductInfo(20));
-        values.put('X', new ProductInfo(17));
-        values.put('Y', new ProductInfo(20));
-        values.put('Z', new ProductInfo(21));
+
+        productInfo = new ProductInfo(17);
+        productInfo.addPackDiscount(packDiscount);
+        values.put('X', productInfo);
+
+        productInfo = new ProductInfo(20);
+        productInfo.addPackDiscount(packDiscount);
+        values.put('Y', productInfo);
+
+        productInfo = new ProductInfo(21);
+        productInfo.addPackDiscount(packDiscount);
+        values.put('Z', productInfo);
     }
 
     public Integer getPriceFor(char item) {
---
+++ src/main/java/befaster/solutions/PackDiscount.java
@@ -0,0 +1,15 @@
+package befaster.solutions;
+
+public class PackDiscount {
+    private final int numberOfItems;
+    private final int pricePerPack;
+    private final char[] items;
+
+    public PackDiscount(int numberOfItems, int pricePerPack, char... items) {
+        this.numberOfItems = numberOfItems;
+        this.pricePerPack = pricePerPack;
+        this.items = items;
+    }
+
+    
+}

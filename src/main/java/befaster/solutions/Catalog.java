1b513b07cdd36761f6504b93d66cd987f7091b41
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 09:37:39 +0000

    Sun Dec 03 09:37:39 GMT 2017

--- src/main/java/befaster/solutions/Catalog.java
+++ src/main/java/befaster/solutions/Catalog.java
@@ -1,4 +1,26 @@
 package befaster.solutions;
 
+import java.util.HashMap;
+
 public class Catalog {
+
+    HashMap<Character, Integer> pricePerProduct;
+    HashMap<Character, Offer> offerPerProduct;
+
+    public Catalog() {
+        pricePerProduct = new HashMap<>();
+        pricePerProduct.put('A', 50);
+        pricePerProduct.put('B', 30);
+        pricePerProduct.put('C', 20);
+        pricePerProduct.put('D', 15);
+
+        offerPerProduct = new HashMap<>();
+        offerPerProduct.put('A', new Offer(3, 130));
+        offerPerProduct.put('B', new Offer(2, 45));
+    }
+
+
+    public Integer getPriceFor(char item) {
+        return pricePerProduct.get(item);
+    }
 }
--- src/main/java/befaster/solutions/Checkout.java
+++ src/main/java/befaster/solutions/Checkout.java
@@ -7,41 +7,33 @@ public class Checkout {
         if (skus.length() == 0) return 0;
         if (!skus.matches("[A-Z]+")) return -1;
 
-        HashMap<Character, Integer> pricePerProduct = new HashMap<Character, Integer>();
-        pricePerProduct.put('A', 50);
-        pricePerProduct.put('B', 30);
-        pricePerProduct.put('C', 20);
-        pricePerProduct.put('D', 15);
+//        HashMap<Character, Integer> pricePerProduct = new HashMap<Character, Integer>();
+//        pricePerProduct.put('A', 50);
+//        pricePerProduct.put('B', 30);
+//        pricePerProduct.put('C', 20);
+//        pricePerProduct.put('D', 15);
 
         HashMap<Character, Offer> offerPerProduct = new HashMap<Character, Offer>();
         offerPerProduct.put('A', new Offer(3, 130));
         offerPerProduct.put('B', new Offer(2, 45));
 
+        Catalog catalog = new Catalog();
         Basket basket = new Basket(skus);
-//        HashMap<Character, Integer> quantityPerProduct = new HashMap<>();
-//        for (char item: skus.toCharArray()) {
-//            if (!quantityPerProduct.containsKey(item)) {
-//                quantityPerProduct.put(item, 1);
-//                continue;
-//            }
-//            int quantity = quantityPerProduct.get(item);
-//            quantityPerProduct.replace(item, quantity + 1);
-//        }
 
         int total = 0;
         for (char item: basket.getItems()) {
-//        for (char item: quantityPerProduct.keySet()) {
             Integer quantity = basket.getQuantityFor(item);
-//            Integer quantity = quantityPerProduct.get(item);
             if (offerPerProduct.containsKey(item)) {
                 Offer offer = offerPerProduct.get(item);
                 if (offer.matches(quantity)) {
                     total += offer.getOfferPrice(quantity);
-                    total += offer.getOutOfOfferProducts(quantity) * pricePerProduct.get(item);
+                    total += offer.getOutOfOfferProducts(quantity) * catalog.getPriceFor(item);
+//                    total += offer.getOutOfOfferProducts(quantity) * pricePerProduct.get(item);
                     continue;
                 }
             }
-            total += quantity * pricePerProduct.get(item);
+            total += quantity * catalog.getPriceFor(item);
+//            total += quantity * pricePerProduct.get(item);
         }
         return total;
     }

b8c8be1a436214c99282ccafdfa565db009c7e32
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 10:54:09 +0000

    Sun Dec 03 10:54:09 GMT 2017

--- src/main/java/befaster/solutions/Discounts.java
+++ src/main/java/befaster/solutions/Discounts.java
@@ -26,6 +26,7 @@ public class Discounts {
     }
 
     public int getAmountToDiscountFor(int numberOfItems) {
+        // TODO: taking into account all discounts
         int packs = numberOfItems / this.values.get(0).getNumberOfItems();
         return packs * this.values.get(0).getQuantityToDiscount();
     }

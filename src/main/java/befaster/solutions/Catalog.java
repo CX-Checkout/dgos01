eab36ffb6b97251d37bd7720198d6d2e53a35cb3
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-03 09:52:39 +0000

    Sun Dec 03 09:52:39 GMT 2017

--- src/main/java/befaster/solutions/Catalog.java
+++ src/main/java/befaster/solutions/Catalog.java
@@ -5,21 +5,22 @@ import java.util.Optional;
 
 public class Catalog {
 
-    HashMap<Character, Integer> pricePerProduct;
-    HashMap<Character, Offer> offerPerProduct;
+//    HashMap<Character, Integer> pricePerProduct;
+//    HashMap<Character, Offer> offerPerProduct;
     HashMap<Character, ProductInfo> values;
 
     public Catalog() {
-        pricePerProduct = new HashMap<>();
-        pricePerProduct.put('A', 50);
-        pricePerProduct.put('B', 30);
-        pricePerProduct.put('C', 20);
-        pricePerProduct.put('D', 15);
+//        pricePerProduct = new HashMap<>();
+//        pricePerProduct.put('A', 50);
+//        pricePerProduct.put('B', 30);
+//        pricePerProduct.put('C', 20);
+//        pricePerProduct.put('D', 15);
+//
+//        offerPerProduct = new HashMap<>();
+//        offerPerProduct.put('A', new Offer(3, 130));
+//        offerPerProduct.put('B', new Offer(2, 45));
 
-        offerPerProduct = new HashMap<>();
-        offerPerProduct.put('A', new Offer(3, 130));
-        offerPerProduct.put('B', new Offer(2, 45));
-
+        values = new HashMap<>();
         values.put('A', new ProductInfo(50, Optional.of(new Offer(3, 130))));
         values.put('B', new ProductInfo(30, Optional.of(new Offer(2, 45))));
         values.put('C', new ProductInfo(20));
